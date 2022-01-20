660a5517e689e9dacc46de09609a287052a5ef82
A-Phamfam <alextampham3000@gmail.com>
2022-01-20 02:25:28 -0600

    hacky solution to min_value and max_value until discord.js is updated
    (cherry picked from commit d44c0534849c2a776e46186bd613661859a01c81)

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,7 +12,33 @@ const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     try {
+        // Temporary hacky fix to discord.js issue with min_value and max_value
+        // Will need to update discord.js in the future to fix this issue.
+        // TODO: Remove this upon updating discord.js to a version where min_value and max_value are fixed.
+        if (command.data.name === 'wrap') {
+            const wrapCommand = {
+                name: 'wrap',
+                description: 'Warps the message sent N messages ago in Matlab backticks ```',
+                options: [
+                    {
+                        max_value: 10,
+                        min_value: 1,
+                        choices: undefined,
+                        autocomplete: undefined,
+                        type: 4,
+                        name: 'n_messages_ago',
+                        description: 'Message to be wrapped in ```',
+                        required: true
+                    }
+                ],
+                default_permission: undefined
+            };
+            commands.push(wrapCommand);
+            continue;
+        }
+
         commands.push(command.data.toJSON());
+
     } catch (error) {
         console.log(`Command for ${file} is not properly formatted.`);
     }
