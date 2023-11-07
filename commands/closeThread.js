1daa2223e2d3b9efe99047d2c0ae7841d779c09f
Sam-Reinsel <62652294+Sam-Reinsel@users.noreply.github.com>
2023-11-07 14:22:29 +0000

    Added closeThread command

---
+++ commands/closeThread.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('closeThread')
+        .setDescription('Close a thread in one of the help forums'),
+    async execute(client, interaction) {
+        const channel = interaction.channel;
+
+        if (channel instanceof TextChannel) {
+            channel.threads.setArchived(true); // archived
+        }       else {
+            interaction.reply('This channel is not a valid channel to use closteThread');
+        }
+            },
+};
